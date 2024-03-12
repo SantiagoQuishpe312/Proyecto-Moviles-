66a6670c72605d77cbb87849cb3051487b865757
[SantiagoQuishpe312] <[sdquishpe1@espe.edu.ec]>
2024-03-12 09:28:41 -0500

    DIR

--- app/src/main/java/com/example/project1752/Activity/ProcesoPedidoActivity.java
+++ app/src/main/java/com/example/project1752/Activity/ProcesoPedidoActivity.java
@@ -2,15 +2,26 @@ package com.example.project1752.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.project1752.R;
 
 public class ProcesoPedidoActivity extends AppCompatActivity {
-
+Button btnHome;
+   // @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proceso_pedido);
+        btnHome=findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(v -> {
+            Intent intent=new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+
     }
+
 }
